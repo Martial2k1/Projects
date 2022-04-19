d44423973ebd2c6b7be466ff6d90ac1b73795176
Martial2k1 <103980986+Martial2k1@users.noreply.github.com>
2022-04-19 13:08:23 +0600

    Add files via upload

---
+++ Mad Libs.py
@@ -0,0 +1,7 @@
+color = input("What color are roses?\n")
+Plurar_noun = input("Enter a plural noun that has the color blue:\n")
+celebrity = input("Enter your favourite celebrity:\n")
+
+print("\nRoses are", color.lower())
+print(Plurar_noun.capitalize(), "are blue")
+print(celebrity.capitalize(), "I love You!")
