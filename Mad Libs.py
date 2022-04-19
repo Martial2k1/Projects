6b87d38bc9867f336f1a3125bb42520f0804adab
Martial2k1 <103980986+Martial2k1@users.noreply.github.com>
2022-04-19 13:08:58 +0600

    Update Mad Libs.py

--- Mad Libs.py
+++ Mad Libs.py
@@ -1,5 +1,5 @@
 color = input("What color are roses?\n")
-Plurar_noun = input("Enter a plural noun that has the color blue:\n")
+plurar_noun = input("Enter a plural noun that has the color blue:\n")
 celebrity = input("Enter your favourite celebrity:\n")
 
 print("\nRoses are", color.lower())
