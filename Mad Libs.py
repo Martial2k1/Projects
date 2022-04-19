26cf13d6818b88832ac86af1d714aa6c33dc20f4
Martial2k1 <103980986+Martial2k1@users.noreply.github.com>
2022-04-19 13:09:30 +0600

    Update Mad Libs.py

--- Mad Libs.py
+++ Mad Libs.py
@@ -3,5 +3,5 @@ plurar_noun = input("Enter a plural noun that has the color blue:\n")
 celebrity = input("Enter your favourite celebrity:\n")
 
 print("\nRoses are", color.lower())
-print(Plurar_noun.capitalize(), "are blue")
+print(plurar_noun.capitalize(), "are blue")
 print(celebrity.capitalize(), "I love You!")
